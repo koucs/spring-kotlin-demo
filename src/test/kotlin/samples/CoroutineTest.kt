dc33e11e547bc4bdd7fe63e3ce919b68c9f0bf9d
koucs <kou.cshu@gmail.com>
2021-12-01 21:14:53 +0900

    Add coroutine code

--- src/test/kotlin/samples/CoroutineTest.kt
+++ src/test/kotlin/samples/CoroutineTest.kt
@@ -1,8 +1,14 @@
 package samples
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
+import samples.coroutine.ContentDuration
 import samples.coroutine.DummyService
+import java.time.Duration
 import java.time.Instant
 
 open class CoroutineTest {
@@ -16,9 +22,24 @@ open class CoroutineTest {
     }
 
     @Test
-    fun should_be_parallel() {
+    fun should_be_parallel2() = runBlocking {
         val start = Instant.now()
+        val results = lambdas.map {
+            async {
+                println(it.content)
+                it.content
+            }
+        }
         val end = Instant.now()
+        results.forEach(::println)
+        Assertions.assertThat(results).isNotNull.isNotEmpty.hasSameSizeAs(services)
+
+        val maxTimeElapsed = results.awaitAll().map { (_, duration) -> duration }.max()
+        println("Time taken by the longest service is  $maxTimeElapsed milliseconds")
+        val duration = Duration.between(start, end)
+        val timeElapsed = duration.toMillis()
+        println("Time taken by the executor service is $timeElapsed milliseconds")
     }
+
 
 }
